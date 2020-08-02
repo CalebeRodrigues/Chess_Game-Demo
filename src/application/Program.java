6ff71af62b108103b3b6abe43a38368054cf67c0
Calebe Rodrigues <calebebrodrigues@gmail.com>
2020-08-01 23:07:57 -0300

    Position class created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
+		
+		//Test Position
+		Position position = new Position(3, 2);
+		System.out.println(position);
 		
 	}
 
